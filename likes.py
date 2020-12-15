b8c3e1b20646eaf7a6c80a92ef805e3b0b2b5bed
Will Koury <wkoury@hey.com>
2020-12-14 21:41:06 -0500

    Initial commit

---
+++ likes.py
@@ -0,0 +1,23 @@
+import tweepy
+import secrets
+import time
+
+auth = tweepy.OAuthHandler(secrets.api_key, secrets.api_key_secret)
+auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+
+api = tweepy.API(auth)
+
+favorites = api.favorites()
+
+while len(favorites) > 0:
+    print("Found ", len(favorites), " tweets.")
+
+    for tweet in favorites:
+        print(tweet.text)
+        print()
+        api.destroy_favorite(tweet.id)
+        time.sleep(2)
+    
+    favorites = api.favorites()
+
+print("No more new tweets to unlike!")
